b3a74f8acc3547ea4387d7f70feea710d4b968c0
vasili <s0569216@htw-berlin.de>
2025-10-16 11:27:17 +0200

    init

---
+++ src/main/java/de/htw/berlin/webtech/recipe_manager/RecipeManagerApplication.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.webtech.recipe_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/berlin/webtech/recipe_manager/RecipeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.webtech.recipe_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
